2fa1b71035535183d5dde31c93711cfec58bae72
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-10-16 22:24:33 -0600

    add function for save local storage and persist

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,26 @@
+
 var currentDate = moment().format("dddd,MMMM Do YYYY");
 $("#currentDay").text(currentDate);
 
+$(document).ready(function () {
+    
+    $(".saveBtn").on("click", function () {
+        var userText = $(this).siblings(".description").val();
+        var timeBlock = $(this).parent().attr("id");
+
+        localStorage.setItem(timeBlock, userText);
+    })
 
 
+    
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+});
